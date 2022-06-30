a8b1a5cc57ad6b9baf6e08ac49bcb3d3fa6bc85a
Matt <97047080+mattxmade@users.noreply.github.com>
2022-06-30 18:27:42 +0100

    Implement hit and isSunk functions; add properties

--- src/ship.js
+++ src/ship.js
@@ -1,17 +1,27 @@
 function ship(length) {
-  const hits = 0;
+  const position = [];
 
-  const _updateHitCount = () => hits++;
+  let shields = length;
 
-  const hit = (position) => {
-    console.log("method called");
+  const hit = (coordinate) => {
+    let confirm = "miss";
+
+    position.forEach((area, index) => {
+      if (area === coordinate) {
+        confirm = "hit";
+        position.splice(index, 1);
+        shields = position.length;
+      }
+    });
+
+    return confirm;
   };
 
   const isSunk = () => {
-    console.log("method called");
+    return shields === 0 ? true : false;
   };
 
-  return { hits, length, hit, isSunk };
+  return { position, hit, isSunk };
 }
 
 module.exports = ship;
