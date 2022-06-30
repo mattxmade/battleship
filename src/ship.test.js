171e3fad253ea9a24dcbe47cd6f76e55e7b73c3e
Matt <97047080+mattxmade@users.noreply.github.com>
2022-06-30 18:26:47 +0100

    Create unit tests for Ship functions

--- src/ship.test.js
+++ src/ship.test.js
@@ -1,14 +1,34 @@
 const ship = require("./ship");
 
-describe("Game Logic Component: Ship", () => {
-  test("should return correct properties", () => {
-    const carrier = ship(5);
+describe("Ship: Initialiser", () => {
+  const cruiser = ship(3);
 
-    expect(carrier).toMatchObject({
-      hits: 0,
-      length: 5,
+  test("valid object properties", () => {
+    expect(cruiser).toMatchObject({
+      position: [],
       hit: expect.any(Function),
       isSunk: expect.any(Function),
     });
+  });
+});
+
+describe("Ship: Functions", () => {
+  const destroyer = ship(2);
+  destroyer.position.push("D7", "E7");
+
+  test("takes a string, confirm miss", () => {
+    expect(destroyer.hit("D3")).toBe("miss");
+  });
+
+  test("takes a string, confirm hit", () => {
+    expect(destroyer.hit("D7")).toBe("hit");
+  });
+
+  test("takes a string, confirm hit", () => {
+    expect(destroyer.hit("E7")).toBe("hit");
+  });
+
+  test("ship sinks when shields at 0", () => {
+    expect(destroyer.isSunk()).toBeTruthy();
   });
 });
