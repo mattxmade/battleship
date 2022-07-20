edf75a86a61f61fdf37701c06621e33ca7b040b3
Matt <97047080+mattxmade@users.noreply.github.com>
2022-07-20 14:55:13 +0100

    Player now has ships/fleet

--- src/player.js
+++ src/player.js
@@ -1,12 +1,29 @@
-import Board from "./gameBoard";
+import gameBoard from "./gameBoard";
 import { v4 as uuidv4 } from "uuid";
+import Ship from "./ship";
 
 function Player() {
   let score = 0;
   const id = uuidv4();
-  const board = Board(id);
+  const board = gameBoard(id);
 
-  return { score, id, board };
+  const ships = {
+    carrierrrr: Ship(5, "carrierrrr"),
+    battleship: Ship(4, "battleship"),
+    cruiserrrr: Ship(3, "cruiserrrr"),
+    submarinee: Ship(3, "submarinee"),
+    destroyerr: Ship(2, "destroyerr"),
+  };
+
+  const fleet = [
+    ships.carrierrrr,
+    ships.battleship,
+    ships.cruiserrrr,
+    ships.submarinee,
+    ships.destroyerr,
+  ];
+
+  return { score, id, board, ships, fleet };
 }
 
-module.exports = Player;
+export default Player;
